5fe52a75fc2bf51b0e149bce9223fc8e2d7af083
Unay Santisteban <usantisteban@othercode.io>
2024-02-13 11:18:21 +1000

    Updated restoration scripts.

--- restoration_scripts/02-python.sh
+++ restoration_scripts/02-python.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 #
 # References:
-#    
+#
 #
 echo -e "\n===== \e[1m\e[97mInstalling Python Interpreter and Poetry\e[0m ====="
-sudo apt install python{3.8,3.9} python{3.8,3.9}-venv
+echo "> Installing Python Repositories"
+sudo add-apt-repository ppa:deadsnakes/ppa
+echo "> Installing Pythpm 3.8, 3.9 and 3.10."
+sudo apt install python{3.8,3.9,3.10} python{3.8,3.9,3.10}-venv
 curl -sSL https://install.python-poetry.org | python3 -
-# add the path to bashrc (by default)
-echo 'export PATH="$HOME/.local/bin:$PATH"' >> .bashrc
