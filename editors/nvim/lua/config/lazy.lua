a7bfd0fe0707a221036439941fc24924ae01a820
Unay Santisteban <usantisteban@othercode.io>
2024-02-19 13:38:00 +1000

    Nvim plugins updated.

--- editors/nvim/lua/config/lazy.lua
+++ editors/nvim/lua/config/lazy.lua
@@ -15,7 +15,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.lang.docker" },
-    { import = "lazyvim.plugins.extras.lang.tex" },
+    -- { import = "lazyvim.plugins.extras.lang.tex" },
     { import = "lazyvim.plugins.extras.lang.python" },
     -- { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "lazyvim.plugins.extras.dap.core" },
