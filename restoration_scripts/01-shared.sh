0bd0943aaf82bb20201947b0578cd3e64d22193d
Unay Santisteban <usantisteban@othercode.io>
2024-02-19 14:09:30 +1000

    Added some shared dependencies.

--- restoration_scripts/01-shared.sh
+++ restoration_scripts/01-shared.sh
@@ -6,4 +6,5 @@
 echo -e "\n===== \e[1m\e[97mInstalling Shared Tools\e[0m ====="
 sudo apt install \
 	ca-certificates curl apt-transport-https lsb-release gnupg mlocate tree sqlite3 libfuse2 \
-	rsync zip unzip dos2unix cowsay gcc g++ make git software-properties-common build-essential
+	rsync zip unzip dos2unix cowsay gcc g++ make git software-properties-common build-essential \
+	fzf fd-find xclip
