5b30c0641d921c8b456170519019da63a8402fe1
Unay Santisteban <usantisteban@othercode.io>
2024-03-10 20:38:33 +1000

    Added new plugins.
    * Added easy-php plugin.
    * Added reinbow-csv plugin.

---
+++ editors/nvim/lua/plugins/easy-php.lua
@@ -0,0 +1,20 @@
+return {
+  "ta-tikoma/php.easy.nvim",
+  config = true,
+  keys = {
+    { "-b", "<CMD>PHPEasyDocBlock<CR>" },
+    { "-r", "<CMD>PHPEasyReplica<CR>" },
+    { "-c", "<CMD>PHPEasyCopy<CR>" },
+    { "-d", "<CMD>PHPEasyDelete<CR>" },
+    { "-ii", "<CMD>PHPEasyInitInterface<CR>" },
+    { "-ic", "<CMD>PHPEasyInitClass<CR>" },
+    { "-iac", "<CMD>PHPEasyInitAbstractClass<CR>" },
+    { "-it", "<CMD>PHPEasyInitTrait<CR>" },
+    { "-ie", "<CMD>PHPEasyInitEnum<CR>" },
+    { "-c", "<CMD>PHPEasyAppendConstant<CR>", mode = { "n", "v" } },
+    { "-p", "<CMD>PHPEasyAppendProperty<CR>", mode = { "n", "v" } },
+    { "-m", "<CMD>PHPEasyAppendMethod<CR>", mode = { "n", "v" } },
+    { "-_", "<CMD>PHPEasyAppendConstruct<CR>" },
+    { "-a", "<CMD>PHPEasyAppendArgument<CR>" },
+  },
+}
